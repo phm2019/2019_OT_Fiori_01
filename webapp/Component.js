2200d3b8d1645fda2b3315ece7f8552e6995f12d
Silvio Haehner <silvio.haehner@x-case.de>
2019-07-02 20:08:21 +0200

    Transfer to MVC w/ controller

---
+++ webapp/Component.js
@@ -0,0 +1,28 @@
+/* global document */
+sap.ui.define([
+		"sap/ui/core/UIComponent"
+	], function (UIComponent) {
+		"use strict";
+		
+		return UIComponent.extend("de.integrata.training.opentext.Component", {
+			metadata : {
+				rootView: {
+					viewName: "de.integrata.training.opentext.view.App",
+					type: "XML"
+				}
+			},
+			
+			/**
+			 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+			 * In this method, the device models are set and the router is initialized.
+			 * @public
+			 * @override
+			 */
+			init : function () {
+				UIComponent.prototype.init.apply(this, arguments);
+				
+				//this.getRouter().initialize();
+			}
+		});
+	}
+);
